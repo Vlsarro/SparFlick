f1d7bfc87c247a6b8bbcfee4008382142caaea3f
Vlsarro <drekmaro@gmail.com>
2016-10-06 16:44:17 +0300

    base version of photogallery

---
+++ public/layouts/admin_header.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>SparFlick: Admin</title>
+    <link href="../stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="header">
+      <h1>SparFlick: Admin</h1>
+    </div>
+    <div id="main">
---
+++ public/layouts/header.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>SparFlick</title>
+    <link href="stylesheets/main.css" media="all" rel="stylesheet" type="text/css" />
+  </head>
+  <body>
+    <div id="header">
+		<!-- <a href="index.php"> -->
+			<h1>SparFlick</h1>
+		<!-- </a> -->
+    </div>
+    <div id="main">
